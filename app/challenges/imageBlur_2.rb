f81c375749ddfeb0021bbe71b440f8724a6dc0ad
Clint Batte <clintbatte@Clints-MacBook-Pro.local>
2019-11-02 16:23:38 -0500

    finish imageblur2

--- app/challenges/imageBlur_2.rb
+++ app/challenges/imageBlur_2.rb
@@ -1,24 +1,3 @@
-# class Image
-
-#     def initialize(image)
-#         @image = image
-#     end
-
-#     def iterate_array
-#         @image.each do |item|
-#             puts item.join(", ")
-#         end
-#     end
-
-#     def output_image
-#         iterate_array
-#     end
-# end
-
-# @imageArray = [[0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]]
-# image = Image.new(@imageArray)
-# image.output_image
-
 class Image
 
   def initialize(image)
@@ -30,30 +9,35 @@ class Image
   def iterate_array
     @pixel.each_with_index do |row, row_index|
       row.each_with_index do |cell, column_index|
-        @coordinates.to_ary << [row_index, column_index] if cell == 1
-
-        if @pixel[row_index, column_index + 1] && [column_index].length.positive?
-          cell == 1
-        end
-        if @pixel[row_index, column_index - 1] && [column_index].length.positive?
-          cell == 1
-        end
-        if @pixel[row_index + 1, column_index] && [row_index].length.positive?
-          cell == 1
-        end
-        if @pixel[row_index - 1, column_index] && [row_index].length.positive?
-          cell == 1
-        end
+        @coordinates << [row_index, column_index] if cell == 1
       end
-      puts @pixel
-      puts @coordinates.join " ,"
     end
+    puts @pixel.inspect
+    puts @coordinates.inspect
   end
 
-  #
-  # def blur_cell
-  #
-  # end
+  def blur_cell
+    @pixel.each_with_index do |row, row_index|
+      row.each_with_index do |_cell, column_index|
+        @coordinates.each do |x, y|
+
+          @width = @pixel[0].count
+          @height = @pixel.count
+
+          next unless x == row_index && y == column_index
+
+          @pixel[row_index - 1][column_index] = 1 unless row_index.zero? # count up
+          @pixel[row_index + 1][column_index] = 1 unless row_index >= @height # count down
+          @pixel[row_index][column_index - 1] = 1 unless column_index.zero? # count left
+          @pixel[row_index][column_index + 1] = 1 unless column_index >= @width - 1 # count right
+        end
+      end
+    end
+
+    @pixel.each do |row|
+      puts row.join
+    end
+  end
 
   def output_image
     iterate_array
@@ -63,21 +47,16 @@ end
 @imageArray = [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]]
 image = Image.new(@imageArray)
 image.output_image
+image.blur_cell
 
-
-# Prime.each_with_index(10) do |prime, i|
-#    puts "#{i}. #{prime}"
-# end
-
-#================================================
-
+# ===============================================
+#  experiments
+# ===============================================
 
 # class Image
-
 #     def initialize(image)
 #         @image = image
 #     end
-
 #     def iterate_array
 #         @image.each_index do |x|
 #             @image.each_index do |y|
@@ -87,7 +66,6 @@ image.output_image
 #             end
 #         end
 #     end
-
 #     def output_image
 #         iterate_array
 #     end
