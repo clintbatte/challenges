da04dee1e7478d263a62a5e8894b9546c6a44c89
Clint Batte <clintbatte@Clints-MacBook-Pro.local>
2019-11-02 13:32:19 -0500

    checkin for now

--- app/challenges/imageBlur_2.rb
+++ app/challenges/imageBlur_2.rb
@@ -2,14 +2,14 @@
 
 #     def initialize(image)
 #         @image = image
-#     end 
-    
+#     end
+
 #     def iterate_array
 #         @image.each do |item|
 #             puts item.join(", ")
 #         end
 #     end
-    
+
 #     def output_image
 #         iterate_array
 #     end
@@ -21,35 +21,48 @@
 
 class Image
 
-    def initialize(image)
-        @image = image
-        @pixel = Marshal.load(Marshal.dump(@image))
-    end
+  def initialize(image)
+    @image = image
+    @pixel = Marshal.load(Marshal.dump(@image))
+    @coordinates = []
+  end
 
-    def iterate_array
-        coordinates = []
-        @pixel.each_with_index do |row, row_index|
-            if row
-            row.each_with_index do |cell, column_index|
-                if cell == 1
-                    coordinates << [row_index, column_index]
-                end
-            end                
+  def iterate_array
+    @pixel.each_with_index do |row, row_index|
+      row.each_with_index do |cell, column_index|
+        @coordinates.to_ary << [row_index, column_index] if cell == 1
+
+        if @pixel[row_index, column_index + 1] && [column_index].length.positive?
+          cell == 1
         end
+        if @pixel[row_index, column_index - 1] && [column_index].length.positive?
+          cell == 1
+        end
+        if @pixel[row_index + 1, column_index] && [row_index].length.positive?
+          cell == 1
+        end
+        if @pixel[row_index - 1, column_index] && [row_index].length.positive?
+          cell == 1
+        end
+      end
+      puts @pixel
+      puts @coordinates.join " ,"
     end
+  end
 
-    def blur_image
-    end
+  #
+  # def blur_cell
+  #
+  # end
 
-    def output_image
-        iterate_array
-    end
+  def output_image
+    iterate_array
+  end
 end
 
-@imageArray = [[0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]]
+@imageArray = [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]]
 image = Image.new(@imageArray)
 image.output_image
-
 
 
 # Prime.each_with_index(10) do |prime, i|
@@ -59,11 +72,10 @@ image.output_image
 #================================================
 
 
-
 # class Image
 
 #     def initialize(image)
-#         @image = image    
+#         @image = image
 #     end
 
 #     def iterate_array
